e7b81755cd12f131a009cfb324394b1dea970c5a
tectiv3 <two@getpanda.me>
2017-02-10 22:05:58 +0000

    encryption module

--- src/Storage.js
+++ src/Storage.js
@@ -1,7 +1,26 @@
-import { AsyncStorage } from 'react-native';
+import { AsyncStorage, NativeModules } from 'react-native';
 var assign = require('object-assign');
 var EventEmitter = require('events').EventEmitter;
 var _notes = null;
+
+var Aes = NativeModules.Aes;
+
+Aes.generateKey("Arnold", function(key) {
+    console.log(key);
+    Aes.encrypt("These violent delights have violent ends", key, function(base64) {
+        console.log(base64);
+        Aes.decrypt(base64, key, function(text) {
+            console.log(text);
+        }, function(){
+            console.log("decrypt error");
+        });
+    }, function() {
+        console.log("encrypt error");
+    });
+}, function() {
+    console.log("generate key error");
+});
+
 
 function loadAsyncStore() {
     return AsyncStorage.getItem('notes').then(str => {
